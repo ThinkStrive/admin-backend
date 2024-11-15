74ef4ecd5bfe0a3b8c1b9df75e233f2e1d0b309e
ThinkStrive <infothinkstrive@gmail.com>
2024-11-16 03:12:03 +0530

    updated

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import Express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { dbConnect } from './DB/connection/Connection.js';
-// import { usersRoute } from './routes/Users.js';
-import { authRoute } from './routes/Authentication.js';
-// import { paymentRouter } from './routes/payment.js';
-// import { checkAndExpireSubscriptions } from './controllers/Schedule.js';
+import { usersRoute } from './routes/Users.js';
+// import { authRoute } from './routes/Authentication.js';
+import { paymentRouter } from './routes/payment.js';
+import { checkAndExpireSubscriptions } from './controllers/Schedule.js';
 
 
 // Config
@@ -27,29 +27,29 @@ const port = process.env.PORT ||''
 app.use(cors())
 app.use(Express.json())
 
-// const allowedOrigins = [
-//   'https://game01-seven.vercel.app',
-//   "https://gaming01.vercel.app",
-//   'http://localhost:5173',
-//   'http://localhost:5174',
-//   'http://localhost:5000',
-//   'https://gamin01.netlify.app',
-//   'https://admin-gaming.netlify.app'
-//   // Add more domains as needed
-// ];
+const allowedOrigins = [
+  'https://game01-seven.vercel.app',
+  "https://gaming01.vercel.app",
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5000',
+  'https://gamin01.netlify.app',
+  'https://admin-gaming.netlify.app'
+  // Add more domains as needed
+];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: 'GET,POST,PUT,DELETE', // Allowed methods
-//   allowedHeaders: 'Content-Type,Authorization', // Headers you want to allow
-//   credentials: true, // Enable if you need cookies/auth headers
-// };
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE', // Allowed methods
+  allowedHeaders: 'Content-Type,Authorization', // Headers you want to allow
+  credentials: true, // Enable if you need cookies/auth headers
+};
 
 app.use(cors());
 
@@ -57,17 +57,17 @@ app.use(cors());
 // API Routes
 // Authentication
 // app.use('/', "welcome")
-app.use('/authentication', authRoute)
+// app.use('/authentication', authRoute)
 
 
 // // Users
-// app.use('/users', usersRoute)
+app.use('/users', usersRoute)
 
 // // Payment
-// app.use('/payment', paymentRouter)
+app.use('/payment', paymentRouter)
 
 
-// checkAndExpireSubscriptions()
+checkAndExpireSubscriptions()
 
 app.get('/', (req, res) => {
   res.send('Welcome')
